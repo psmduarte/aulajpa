ae3259d0e195b25045e7a8d6b2be499f24468fdb
psmduarte <psmduarte@live.com>
2018-04-02 19:59:21 +0100

    Apagar uma pessoa do banco de dados.

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -15,8 +15,14 @@ public class Programa {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Pessoa p = em.find(Pessoa.class, 2);
-		
+		//para apagar pessoas da base de dados é necessario monitorar o objecto pessoa
+		//ou seja, ou é uma pessoa acabada de inserir ou então tem de ser fazer um find
+		//da pessoa.
+		Pessoa p = em.find(Pessoa.class, 1);
+		em.getTransaction().begin(); //para realizar alterações na base de dados é 
+									 //preciso iniciar e comitar a transação
+		em.remove(p);
+		em.getTransaction().commit();//commit da transação
 		System.out.println(p);
 		
 		System.out.println("FEITO!");
