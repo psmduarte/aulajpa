7e0822b966655384cc5d6201975f0504a2359452
psmduarte <psmduarte@live.com>
2018-04-02 19:35:07 +0100

    Add 3 pessoas à base de dados.

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,5 +1,9 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
@@ -7,9 +11,24 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Pessoa p1 = new Pessoa(1,"Paulo Duarte", "psmduarte@live.com");
-		Pessoa p2 = new Pessoa(2,"Daniela Chambel", "danielachambel@hotmail.com");
-		Pessoa p3 = new Pessoa(3,"Maria Duarte","não tem email");
+		Pessoa p1 = new Pessoa(null,"Paulo Duarte", "psmduarte@live.com");
+		Pessoa p2 = new Pessoa(null,"Daniela Chambel", "danielachambel@hotmail.com");
+		Pessoa p3 = new Pessoa(null,"Maria Duarte","não tem email");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("FEITO!");
+		
+		em.close();
+		emf.close();
+		
+		
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
