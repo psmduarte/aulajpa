35ba187a26574df1c0d7bf5197c08da765e6eac9
psmduarte <psmduarte@live.com>
2018-04-02 19:46:10 +0100

    Pesquisar pessoas do banco de dados.

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -11,27 +11,20 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Pessoa p1 = new Pessoa(null,"Paulo Duarte", "psmduarte@live.com");
-		Pessoa p2 = new Pessoa(null,"Daniela Chambel", "danielachambel@hotmail.com");
-		Pessoa p3 = new Pessoa(null,"Maria Duarte","não tem email");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		System.out.println("FEITO!");
+		Pessoa p = em.find(Pessoa.class, 2);
 		
+		System.out.println(p);
+		
+		System.out.println("FEITO!");
 		em.close();
 		emf.close();
 		
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+
 		
 	}
 
